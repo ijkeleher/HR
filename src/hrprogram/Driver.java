cc3d6a8303f2405a4cebc53287842ca9b4d4fc09
dhirajskr <dhirajskr@gmail.com>
2018-05-13 21:19:48 +1000

    updated my driver class

--- src/hrprogram/Driver.java
+++ src/hrprogram/Driver.java
@@ -52,16 +52,13 @@ public class Driver {
             //course, coordinator and schedule pre-created for testing purposes
 
             Course SEFCOSC2537 = new Course("SEFCOSC253");
-            CourseCoordinator Andrew = new CourseCoordinator("Andrew", "e4545416");
+            CourseCoordinator Andrew = new CourseCoordinator("Andrew", "4545416");
             Timetable SEFschedule = new Timetable(SEFCOSC2537, Andrew);
             
             
-            //load();
+            load();
             Driver d = new Driver(); 
             
-            Scanner inputChoice = new Scanner(System.in);
-            Scanner inputUserName = new Scanner(System.in);
-            Scanner inputPassword = new Scanner(System.in);
             
             System.out.println("*** Welcome to HR Management System. ***" + "\n" + "\n" + "\n");
             System.out.println("If you have an existing account            1" + "\n");
@@ -74,98 +71,150 @@ public class Driver {
             
             do {
 
-                System.out.println("Enter a value");
+                System.out.println("Enter a menu value");
                 enteredValue = menuValue.next();
 
                 switch (enteredValue) {
                 case "1":
                     System.out.println("Existing Account");
-
-                    System.out.println("Enter the username: ");
-                    d.username = inputUserName.nextLine();      
-
-                    if (d.username.isEmpty()) {
-                        System.out.println("\nNothing was entered for the UserName, Please try again.");
-                        d.username = inputUserName.nextLine();
-                    }
-                    
-                    if (!(d.username.isEmpty()) && d.userNameDuplicate(d.username)){
-                        System.out.println("Enter the password: ");
-                        d.password = inputPassword.nextLine();
-                                            
-                        //check if password is correct 
-                        System.out.println(d.getUserInfo(d.username).getPassword());
-                        if (d.password.equals(d.getUserInfo(d.username).getPassword())){
-                            // password matches
-                            System.out.println("You are logged in");
-                        } else {
-                            System.out.println("Password does not match ");
-                        }
-                        
-                    } else {
-                        System.out.println("This username does not exist");
-                    }
-
-                    
+                    d.getAccounts();
                     break;
                 case "2":
                     System.out.println("Creating Account");
-
-                    System.out.println("Enter the username: ");
-                    d.username = inputUserName.nextLine();      
-
-                    if (d.username.isEmpty()) {
-                        System.out.println("\nNothing was entered for the UserName, Please try again.");
-                        d.username = inputUserName.nextLine();
-                    }
-
-                    //check for username duplicity 
-                    if (!(d.username.isEmpty()) && !(d.userNameDuplicate(d.username))){
-                        //continue
-                        
-                        System.out.println("Enter the password: ");
-                        d.password = inputPassword.nextLine();
-                    } else {
-                        System.out.println("UserName already exist, Choose another User Name");
-                    }
-            
-                    //adding user details to the array list 
-                    Driver d1 = new Driver(d.username,d.password); 
-                    d.userInfoArray.add(d1);    
+                    d.addAccounts();
                     break;
-
                 case "X":
                 case "x":
-/*
+
                     try{
-                    FileWriting();
+                    fileWriting();
                     System.out.println("Save and Exit");
                     } catch (FileNotFoundException fe){
                         //throw OfferException("File Not Found");
                         System.out.println("File Not Found" );
                     }
-*/              }               
+             }               
 
             } while (!(enteredValue.toUpperCase().equals("X")));
 
             
         }
         
+        public static void fileWriting() throws FileNotFoundException {
+    		// TODO Auto-generated method stub
+    		String filename = "backup.txt"; // path & filename
+    		PrintWriter outputStream = null;
+
+    		try {
+    			// create a new object of the printWriter class & assign to the
+    			// Object variable
+    			outputStream = new PrintWriter(new FileOutputStream(filename));
+    			
+    			for (Driver driver : userInfoArray) {
+    				outputStream.println();
+	    			outputStream.print("*User Name*,");
+	    			outputStream.print(driver.getUserName().toUpperCase() + ",");
+	    			//outputStream.print("*Password*,");
+	    			outputStream.print(driver.getPassword() + ",");
+    			}
+    			
+    		} catch (FileNotFoundException e) {
+    			// display the inbuilt error message belonging to e object
+    			System.out.println(e);
+    		} // end try catch
+    		outputStream.close(); // close the stream
+    		
+        }
         
+        
+        public void addAccounts(){
+        	
+            Scanner inputChoice = new Scanner(System.in);
+            Scanner inputUserName = new Scanner(System.in);
+            Scanner inputPassword = new Scanner(System.in);
+
+            System.out.println("Enter the username: ");
+            String entValue_userName = inputUserName.nextLine();      
+
+            if (entValue_userName.isEmpty()) {
+                System.out.println("\nNothing was entered for the UserName, Please try again.");
+                entValue_userName = inputUserName.nextLine();
+            }
+
+            //check for username duplicity 
+            if (!(entValue_userName.isEmpty()) && !(userNameDuplicate(entValue_userName.toUpperCase()))){
+                System.out.println("Enter the password: ");
+                String entValue_password = inputPassword.nextLine();
+
+                //adding user details to the array list 
+                Driver d1 = new Driver(entValue_userName,entValue_password); 
+                userInfoArray.add(d1);    
+    			System.out.println("New Account Added " + entValue_userName);
+
+            } else {
+                System.out.println("UserName already exist, Choose another User Name");
+            }
+    
+			
+        }
+        
+        public void getAccounts(){
+        
+            Scanner inputChoice = new Scanner(System.in);
+            Scanner inputUserName = new Scanner(System.in);
+            Scanner inputPassword = new Scanner(System.in);
+
+            System.out.println("Enter the username: ");
+            String entValue_userName = inputUserName.nextLine();      
+
+            if (entValue_userName.isEmpty()) {
+                System.out.println("\nNothing was entered for the UserName, Please try again.");
+                entValue_userName = inputUserName.nextLine();
+            }
+            
+            //check if existing
+            if (!(entValue_userName.isEmpty()) && (userNameDuplicate(entValue_userName.toUpperCase()))){
+                System.out.println("Enter the password: ");
+                String entValue_password = inputPassword.nextLine();
+
+				Driver thisDriver = getDriverDetails(entValue_userName.toUpperCase());
+
+                //check if password is correct 
+                System.out.println(thisDriver.getPassword());
+                if (thisDriver.getPassword().equals(entValue_password)){
+                	// password matches
+                    System.out.println("You are logged in");
+                } else {
+                    System.out.println("Password does not match ");
+                }
+                
+            } else {
+                System.out.println("This username does not exist");
+            }
+
+        }
+        
+        
+    	public Driver getDriverDetails(String arg_userName) {
+    		for (Driver d : userInfoArray) {
+    			if (d.getUserName().toUpperCase().equals(arg_userName)) {
+    	    		System.out.println("Found somethin  :) ");
+    				return d;
+    			}
+    		}
+    		System.out.println("Nothing to return :( ");
+    		return null;
+    	}
+    	
         //checks if the username exists in the arraylist 
-        
         public boolean userNameDuplicate(String arg_username) {
 
             boolean userNameFound = false;
-            
-            //System.out.println("In if duplicate check Array Size"+userInfoArray.size());
-            
+          
             for (int i = 0; i < userInfoArray.size(); i++) {
 
                 String currentUserName = ((Driver) userInfoArray.get(i)).getUserName();
-
                 if (arg_username.toUpperCase().equals(currentUserName.toUpperCase())) {
-
                     userNameFound = true;
                     break;
 
@@ -173,51 +222,9 @@ public class Driver {
                     userNameFound = false;
                 }
             }
-
             return userNameFound;
         }
 
-
-        public Driver getUserInfo(String arg_username) {
-            for (Driver d0 : userInfoArray) {
-                if (d0.getUserName().toUpperCase().equals(arg_username.toUpperCase())) {
-                    return d0;
-                }
-            }
-
-            return null;
-        }
-        
-        
-        /*Writing arraylist into a file
-         * 
-         */
-        public static void FileWriting() throws FileNotFoundException {
-            // TODO Auto-generated method stub
-            String filename = "backup.txt"; // path & filename
-            //C:\Users\Dhiraj\workspace\Sem2\SEF
-            PrintWriter outputStream = null;
-
-            try {
-                // create a new object of the printWriter class & assign to the
-                // Object variable
-                outputStream = new PrintWriter(new FileOutputStream(filename));
-
-                for (Driver d0 : userInfoArray) {
-                        outputStream.print(d0.getUserName()+ ",");
-                        outputStream.print(d0.getPassword()); 
-                        outputStream.println();
-                }
-                System.out.println("Yeah! File was written to without error");
-
-            } catch (FileNotFoundException e) {
-                // display the inbuilt error message belonging to e object
-                System.out.println(e);
-            } // end try catch
-            outputStream.close(); // close the stream
-        }
-
-        
         //load values from the txt file 
         private static void load() {
             Scanner inputStream = null; // create object variable
@@ -233,14 +240,15 @@ public class Driver {
 
             try{
                 while (inputStream.hasNext()) {
-                            System.out.println(inputStream.toString());
+                	if (!(inputStream.next().isEmpty())){
                           String readUserName = inputStream.next();
                           String readPassword = inputStream.next();
                           
                           Driver d1 = new Driver(readUserName, readPassword);
                           
                           userInfoArray.add(d1);
- 
+    			         // System.out.println("Read an User Account: "+ readUserName + " " + readPassword);
+                	}
                 } 
             } catch (IllegalStateException e){
                 System.err.println("Could not read from the file");
@@ -251,27 +259,35 @@ public class Driver {
         }       
 
     
+        /*
+        public Driver getUserInfo(String arg_username) {
+            for (Driver d0 : userInfoArray) {
+                if (d0.getUserName().toUpperCase().equals(arg_username.toUpperCase())) {
+                    return d0;
+                }
+            }
+
+            return null;
+        }
+         */        
+        
    void modifyTimetable(Timetable timetable){
        
        //use the createClass, removeClass and listClass here to make timetable changes
        //for each course
 
-       //each course has a single timetable object
-       //the timetable object contains a hashmap called tutorialsMap containing
-       //all of the tutorials for that course
-       
-       //the course has a hashmap containing all the imported casual objects
         
     }
    
   
   void createTimeSheet(){
       
-      //timesheet constructor takes a tutorial object as input
-      
       
   }
   
+  void modifyTimesheet(TimeSheet timesheet){
+      
+  }
   
   void deleteTimeSheet(TimeSheet timesheet){
       
@@ -279,10 +295,11 @@ public class Driver {
   
   void createReport(){
       
-      //report constructor takes a course object as input
-      
   }
   
+  void modifyReport(Report Report){
+      
+  }
   
   void deleteReport(Report report){
       
